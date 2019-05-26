eac6be62393d8f8c79b9e732fead2fda8daeef13
mon7792 <monish7792@gmail.com>
2019-05-26 10:35:01 +0000

    Uploading the Aetherchain pdf

--- src/app.js
+++ src/app.js
@@ -89,7 +89,7 @@ App = {
     web3.eth.getCoinbase(function(err, account) {
       if (err === null) {
         App.account = account;
-        $("#accountAddress").html("Your Account: " + account);
+        // $("#accountAddress").html("Your Account: " + account);
       }
     });
 
